121a3269bed19818247c705fe77a5268e40cb995
dransom90 <dransom90@yahoo.com>
2021-02-15 14:51:23 -0700

    Basic Functionality
    User can add new items

---
+++ Model/FoodItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Food_Storage_Inventory.Model
+{
+	public sealed class FoodItemRepository
+	{
+		private static readonly Lazy<FoodItemRepository> lazy = new Lazy<FoodItemRepository>(() => new FoodItemRepository());
+
+		public static FoodItemRepository Instance => lazy.Value;
+
+		private FoodItemRepository()
+		{
+			FoodItems = new ObservableCollection<FoodItem>()
+			{
+				new FoodItem(){Name = "Blackberry Jam", Quantity = 5, ContainerDescription = "Pint Jars"},
+				new FoodItem(){Name = "Peaches Jam", Quantity = 3, ContainerDescription = "Quart Jars"},
+			};
+		}
+
+		public ObservableCollection<FoodItem> FoodItems { get; set; }
+	}
+}
---
+++ ViewModel/GeneralInventoryViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using Food_Storage_Inventory.Model;
+using Prism.Commands;
+
+namespace Food_Storage_Inventory.ViewModel
+{
+	public class GeneralInventoryViewModel : INotifyPropertyChanged
+	{
+		public GeneralInventoryViewModel()
+		{
+			//FoodItems = FoodItemRepository.Instance.FoodItems;
+		}
+
+		//public List<ObservableCollection> FoodItems { get; set; }
+
+		public string NewItemName { get; set; }
+		public string NewItemQuantity { get; set; }
+		public string NewItemContainerDescription { get; set; }
+		public string SelectedOption { get; set; }
+
+		public ICommand AddNewItemCommand => new DelegateCommand<object>(OnNewItemExecuted);
+		public ICommand NewItemButtonCommand => new DelegateCommand<object>(OnNewItemButtonExecuted);
+
+		public ICommand UpdateItemCommand => new DelegateCommand<object>(OnUpdateItemExecuted);
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnNewItemExecuted(object context)
+		{
+			FoodItemRepository.Instance.FoodItems.Add(new FoodItem() { Name = NewItemName, Quantity = int.Parse(NewItemQuantity), ContainerDescription = NewItemContainerDescription });
+
+			//PropertyChanged(this, new PropertyChangedEventArgs(nameof(FoodItems)));
+		}
+
+		private void OnNewItemButtonExecuted(object context)
+		{
+			NewItemPopup newItemPopup = new NewItemPopup();
+			newItemPopup.Show();
+		}
+
+		private void OnUpdateItemExecuted(object context)
+		{
+		}
+	}
+}
