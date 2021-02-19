a3c676f7243d9f26cc70c449e277ac0203dd9fec
dransom90 <dransom90@yahoo.com>
2021-02-19 10:50:47 -0700

    Customize Containers

--- Model/FoodItem.cs
+++ Model/FoodItem.cs
@@ -1,10 +1,21 @@
-﻿namespace Food_Storage_Inventory.Model
+﻿using System;
+
+namespace Food_Storage_Inventory.Model
 {
 	public class FoodItem
 	{
-		public string Name { get; set; }
+		private string _name { get; set; }
 		public int Quantity { get; set; }
+		public string Container { get; set; }
+		public string Name { get => ToString(); }
 
-		public override string ToString() => Name;
+		public override string ToString() => $"{_name} - {Container}";
+
+		public FoodItem(string name, int quantity, string container)
+		{
+			_name = name ?? throw new ArgumentNullException(nameof(name));
+			Quantity = quantity;
+			Container = container ?? throw new ArgumentNullException(nameof(container));
+		}
 	}
 }
--- ViewModel/NewItemViewModel.cs
+++ ViewModel/NewItemViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows.Input;
 using Food_Storage_Inventory.Model;
@@ -8,13 +9,66 @@ namespace Food_Storage_Inventory.ViewModel
 {
 	public class NewItemViewModel : INotifyPropertyChanged
 	{
+		private string _selectedContainer;
+
 		public string FeedbackText { get; set; } = string.Empty;
 		public string NewItemName { get; set; }
 		public string NewItemQuantity { get; set; }
+		public bool InputDialogVisible { get; set; }
+
+		public string SelectedContainer
+		{
+			get => _selectedContainer;
+			set
+			{
+				if (_selectedContainer == value)
+				{
+					return;
+				}
+
+				if (value == "Create New Container")
+				{
+					InputDialogVisible = true;
+					PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputDialogVisible)));
+					return;
+				}
+
+				_selectedContainer = value;
+				PropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedContainer)));
+			}
+		}
+
+		public ObservableCollection<string> AvailableContainers { get; set; }
 
 		public ICommand AddNewItemCommand => new DelegateCommand<object>(OnNewItemExecuted);
+		public ICommand AddNewContainerCommand => new DelegateCommand<object>(OnNewContainerAdded);
+		public ICommand CancelNewContainerCommand => new DelegateCommand<object>(OnCancelNewContainer);
 
 		public event PropertyChangedEventHandler PropertyChanged;
+
+		public NewItemViewModel()
+		{
+			AvailableContainers = new ObservableCollection<string>() { "Create New Container" };    // TODO:	Read From File
+		}
+
+		private void OnCancelNewContainer(object context)
+		{
+			InputDialogVisible = false;
+			PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputDialogVisible)));
+		}
+
+		private void OnNewContainerAdded(object context)
+		{
+			if (context is string newContainer)
+			{
+				AvailableContainers.Add(newContainer);
+				InputDialogVisible = false;
+				PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputDialogVisible)));
+				PropertyChanged(this, new PropertyChangedEventArgs(nameof(AvailableContainers)));
+			}
+
+			// TODO:	Save To File
+		}
 
 		private void OnNewItemExecuted(object context)
 		{
@@ -32,7 +86,7 @@ namespace Food_Storage_Inventory.ViewModel
 				PropertyChanged(this, new PropertyChangedEventArgs(nameof(FeedbackText)));
 			}
 
-			FoodItemRepository.Instance.FoodItems.Add(new FoodItem() { Name = NewItemName, Quantity = int.Parse(NewItemQuantity) });
+			FoodItemRepository.Instance.FoodItems.Add(new FoodItem(NewItemName, int.Parse(NewItemQuantity), SelectedContainer));
 		}
 	}
 }
