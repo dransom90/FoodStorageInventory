2511a2b444ff333c04955d8f0ee13b92fd966bcb
dransom90 <dransom90@yahoo.com>
2021-02-21 14:39:26 -0700

    Created a Main Menu
    Introducing a main menu.  The inventory function will now reside in  its own window.

---
+++ ViewModel/MainMenuViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Food_Storage_Inventory.Model;
+using Prism.Commands;
+
+namespace Food_Storage_Inventory.ViewModel
+{
+	public class MainMenuViewModel
+	{
+		public ICommand ExitProgramCommand => new DelegateCommand<object>(OnExitProgram);
+		public ICommand SaveCommand => new DelegateCommand<object>(OnSaveExecuted);
+		public ICommand BackUpCommand => new DelegateCommand<object>(OnBackupExecuted);
+		public ICommand OverviewCommand => new DelegateCommand<object>(OnOverviewExecuted);
+		public ICommand SearchCommand => new DelegateCommand<object>(OnSearchExecuted);
+		public ICommand InventoryCommand => new DelegateCommand<object>(OnInventoryExecuted);
+
+		private void OnExitProgram(object context) => Application.Current.Shutdown();
+
+		private void OnSaveExecuted(object context) => FoodItemRepository.Instance.SaveToFile();
+
+		private void OnBackupExecuted(object context) => FoodItemRepository.Instance.BackupFile();
+
+		private void OnOverviewExecuted(object context)
+		{
+		}
+
+		private void OnSearchExecuted(object context)
+		{
+		}
+
+		private void OnInventoryExecuted(object context)
+		{
+		}
+	}
+}
